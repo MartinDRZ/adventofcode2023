04986c0b165aef1cf84aa973cde81298d5793c21
Martin <martindures@gmail.com>
2023-12-04 20:53:58 +0000

    day1 almost

--- day1/trebuchet.py
+++ day1/trebuchet.py
@@ -1,1 +1,11 @@
 #Solution to Day 1?
+
+import re
+
+with open("/home/martin/git-clones/adventofcode2023/files/md_files/day1_input","r") as calnum:
+    for line in calnum:
+        nums = re.findall("[0-9]", line)
+        lastnumindex = len(nums) - 1
+        firstnum = nums[0]
+        lastnum = nums[lastnumindex]
+        final num = 
