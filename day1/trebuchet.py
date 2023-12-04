0d2e69a98c590d77fbbe4d3b9401298fd866a102
Martin <martindures@gmail.com>
2023-12-04 20:59:11 +0000

    Finished Day 1 P1

--- day1/trebuchet.py
+++ day1/trebuchet.py
@@ -2,10 +2,15 @@
 
 import re
 
+total = 0
+
 with open("/home/martin/git-clones/adventofcode2023/files/md_files/day1_input","r") as calnum:
     for line in calnum:
         nums = re.findall("[0-9]", line)
         lastnumindex = len(nums) - 1
         firstnum = nums[0]
         lastnum = nums[lastnumindex]
-        final num = 
+        finalnum = int(str(firstnum) + str(lastnum))
+        print(finalnum)
+        total = total + finalnum
+        print(total)
