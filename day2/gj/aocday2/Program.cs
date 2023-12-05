d1cbb2425ba707e8702486c96575e24d3dc8fa61
Gareth James <81640442+gareth5james@users.noreply.github.com>
2023-12-05 19:36:51 +0000

    tried c# and got confused

---
+++ day2/gj/aocday2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using Microsoft.VisualBasic;
+
+String line;
+var inputList = new List<string>();
+String[] inputArray;
+String[][] formattedArray;
+
+try
+{
+    //Pass the file path and file name to the StreamReader constructor
+    StreamReader sr = new StreamReader("input.txt");
+    //Read the first line
+    line = sr.ReadLine();
+    //Continue to read lines until there are no more lines
+    while (line != null)
+    {
+        //add the line to the list
+        inputList.Add(line);
+        //Read the next line
+        line = sr.ReadLine();
+    }
+    //close the file
+    sr.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
+finally
+{
+    Console.WriteLine("Input file read");
+}
+
+inputArray = inputList.ToArray();
+
+Console.WriteLine("Array Created");
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+
+}
